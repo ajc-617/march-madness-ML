232a14216d5a389a4035ad6536e82084616c42d2
Andrew Childs <ajchilds@gmail.com>
2023-03-20 15:42:09 -0400

    completed main loop, still need to make a table from dictionary

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-page_paths = {'Assist/Turnover Ratio': '474', 'Assists/Game': '216', 'Bench Points/Game': '1284', 'Blocks/Game': }
+page_paths = {'Assist/Turnover Ratio': '474', 'Assists/Game': '216', 'Bench Points/Game': '1284', 'BKPG': '214', 'Effective FG Pct': '1288', 'Fastbreak PTS': '1285', 'FG Pct': '148', 'FG Pct Defense': '149', \
+              'Fouls/Game': '286', 'FT Attempts/Game': '638', 'FT Pct': '150', 'FT Made/game': '633', 'OPP RPG': '151', 'DRPG/Game': '859', 'ORPG/Game': '857', 'RPG': '932', 'OPP PPG': '146', \
+              'PPG': '145', 'STPG': '215', '3FGA/Game': '625', '3FG Pct': '152', 'OPP 3FG Pct': '518', '3PG': '153', 'TO Margin': '519', 'OPP TO/Game': '931', 'TO/Game': '217', 'Win Pct': '168'}
---
+++ testing.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+from main import page_paths
+import requests
+
+base_url = "https://www.ncaa.com/stats/basketball-men/d1/current/team"
+
+team_stats_dictionary = {}
+
+first_run = True
+num_teams = 0
+num_iter = 0
+
+for cur_key in page_paths:
+    if num_iter == 12:
+        print("jere")
+    new_url = base_url + "/" + page_paths[cur_key]
+    for cur_page in range (1, 9):
+        actual_new_url = new_url + "/" + "p" + str(cur_page)
+        cur_page = requests.get(actual_new_url)
+        doc = BeautifulSoup(cur_page.text, "html.parser")
+        rows = doc.find_all("tr")
+        rows = rows[1:len(rows)]
+        if cur_page == 8:
+            print(rows)
+            exit(0)
+        for cur_row in rows:
+            entries = cur_row.find_all("td")
+            if cur_key != 'Fouls/Game' and cur_key != 'OPP RPG':
+                cur_stat = entries[len(entries) - 1].string
+            else:
+                cur_stat = entries[len(entries) - 2].string
+            cur_team_name = cur_row.find("a").string
+            if first_run:
+                team_stats_dictionary[cur_team_name] = {cur_key : cur_stat}
+                continue
+            team_stats_dictionary[cur_team_name][cur_key] = cur_stat
+    first_run = False
+    num_iter += 1
+            
